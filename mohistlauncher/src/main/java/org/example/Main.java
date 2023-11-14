c1d23e7b7a6498cfe9983b64429320dc56e6a48c
Mgazul <mgazul@163.com>
2023-11-14 21:40:59 +0800

    Clean file

---
+++ mohistlauncher/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author Mgazul by MohistMC
+ * @date 2023/11/14 21:19:14
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
